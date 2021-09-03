27cc40872b97d98965957921647facbe2c6284f4
Jonathan Dagan <jonathandagan12@gmail.com>
2021-09-03 17:38:36 +0300

    finished cursor_handler (for now at least)

--- widgets/screen_viewer/cursor_handler.py
+++ widgets/screen_viewer/cursor_handler.py
@@ -13,18 +13,17 @@ class CursorHandler:
         self.cursor.left_click()
 
     def get_pos(self, x, y):
-        self.cursor.set_pos()
+        self.cursor.get_pos()
 
     def set_pos(self, x, y):
         self.cursor.set_pos(x,y)
 
-    def motion(self, event):
-        x, y = event.x, event.y
-        self._relative_pos(x,y)
-        # print('{}, {}'.format(x, y))
-        # TODO: have this trigger a func to send cordinate pos
+    def motion(self, event, viewport: Screen, remote: Screen):
+        p = Point (event.x, event.y)
+        point = self._relative_pos(p,viewport,remote)
+        self.set_pos(point.getX, point.getY)
     
-    def _relative_pos(self, p: Point, viewport: Screen, remote: Screen):
+    def _relative_pos(self, p: Point, viewport: Screen, remote: Screen) -> Point:
         x = p.getX * (remote.getX/viewport.getX)
         y = p.getY * (remote.getY/viewport.getY)
         return Point(x, y)
